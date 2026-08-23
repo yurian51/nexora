9b545efeb7d1a5afac2826abeb2a57d360d7d572
yurian51 <yurianarshad@gmail.com>
2026-08-23 13:35:33 +0300

    feat(api): register database module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, HealthModule],
 })
 export class AppModule {}
