8bc286125ae472ea47e5a0d89e23c655ad9785c2
yurian51 <yurianarshad@gmail.com>
2026-08-25 12:11:19 +0300

    feat(billing): register billing module

---
+++ apps/api/src/billing/billing.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
+
+@Module({ controllers: [BillingController], providers: [BillingService], exports: [BillingService] })
+export class BillingModule {}
