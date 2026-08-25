074e97eb8e4eb6286d2074eea2768400adb6a160
yurian51 <yurianarshad@gmail.com>
2026-08-25 10:15:12 +0300

    feat(api): wire plans and subscriptions modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,8 +5,10 @@ import { CustomersModule } from './customers/customers.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
 import { OverviewModule } from './overview/overview.module';
+import { PlansModule } from './plans/plans.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, HealthModule, OverviewModule, CustomersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, HealthModule, OverviewModule, CustomersModule, PlansModule, SubscriptionsModule],
 })
 export class AppModule {}
