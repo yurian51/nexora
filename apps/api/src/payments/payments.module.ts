1cbbea13421416bc281208a9c9628f9ba4675282
yurian51 <yurianarshad@gmail.com>
2026-09-03 13:47:22 +0300

    feat(payments): register payment module

---
+++ apps/api/src/payments/payments.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+
+@Module({ controllers: [PaymentsController], providers: [PaymentsService], exports: [PaymentsService] })
+export class PaymentsModule {}
