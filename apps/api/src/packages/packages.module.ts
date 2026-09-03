530cb9eec91994cfef725d0886dacae4f3f20d23
yurian51 <yurianarshad@gmail.com>
2026-09-03 13:30:46 +0300

    feat(packages): register package module

---
+++ apps/api/src/packages/packages.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { PackagesController } from './packages.controller';
+import { PackagesService } from './packages.service';
+
+@Module({ controllers: [PackagesController], providers: [PackagesService], exports: [PackagesService] })
+export class PackagesModule {}
